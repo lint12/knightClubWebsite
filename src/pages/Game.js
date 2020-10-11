3170f0fee7cc2375347998c66521be403170f69d
Tiffany Lin <tiffanylin@knights.ucf.edu>
2020-10-11 05:41:53 -0400

    added an alert msg to game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
 import Unity, { UnityContext } from "react-unity-webgl";
+import Alert from '@material-ui/lab/Alert';
+import { Row } from 'reactstrap'; 
 
 class Game extends Component {
   constructor() {
     super();
+
+    this.state = {
+      alert: false
+    }
 
     this.unityContext = new UnityContext({
       loaderUrl: "Builds/Build/Builds.loader.js",
@@ -14,12 +20,31 @@ class Game extends Component {
     });
   }
 
+  componentDidMount() {
+    this.setState({ alert: true }, ()=>{
+      window.setTimeout(()=>{
+        this.setState({ 
+          alert: false, 
+        })
+      },1000)
+    }); 
+  }
+
   render() {
     console.log("Context: ", this.unityContext); 
     return (
       <div>
         <Nav />
-        <div style={{display: "flex", justifyContent: "center"}}>
+        <Row style={{display: "flex", justifyContent: "center"}}>
+        {
+          this.state.alert ? 
+          <Alert severity="info" style={{width: "70%"}}>
+            Please wait a second for the Unity Game to load.
+          </Alert> 
+          : null
+        } 
+        </Row>
+        <div style={{display: "flex", justifyContent: "center"}} id="game-container">
           <Unity unityContext={this.unityContext} />
         </div>
       </div>
